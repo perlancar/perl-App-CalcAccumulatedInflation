630071926237fd949920696fcff871fd151abc6b
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2021-04-01 20:23:23 +0700

    [ux] Add more examples

--- lib/App/CalcAccumulatedInflation.pm
+++ lib/App/CalcAccumulatedInflation.pm
@@ -13,12 +13,13 @@ our %SPEC;
 
 $SPEC{calc_accumulated_inflation} = {
     v => 1.1,
-    summary => 'Calculate accumulated inflation over the years',
+    summary => 'Calculate accumulated inflation (or savings rate, etc) over the years',
     description => <<'_',
 
 This routine generates a table of accumulated inflation over a period of several
 years. You can either specify a fixed rate for every years (`yearly_rate`), or
-specify each year's rates (`rates`).
+specify each year's rates (`rates`). You can also optionally set base index
+(default to 1) and base year (default to 0).
 
 _
     args => {
@@ -53,11 +54,19 @@ _
             args => {yearly_rate=>6},
         },
         {
+            summary => 'See accumulated 5.5%/year inflation for 7 years',
+            args => {yearly_rate=>5.5, years=>7},
+        },
+        {
             summary => "Indonesia's inflation rate for 2003-2014",
             args => {rates=>[5.16, 6.40, 17.11, # 2003-2005
                              6.60, 6.59, 11.06, 2.78, 6.96, # 2006-2010
                              3.79, 4.30, 8.38, 8.36, # 2011-2014
                          ]},
+        },
+        {
+            summary => 'How much will your $100,000 grow over the next 10 years, if the savings rate is 4%; assuming this year is 2021',
+            args => {yearly_rate=>4, years=>10, base_year=>2021, base_index=>100000},
         },
     ],
     result_naked => 1,
